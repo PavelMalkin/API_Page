a446f0fa9d32352fdb13a2b73f77d9d3f9fb1132
Pavel Malkin <frontenddev7@yieldx.app>
2021-11-22 00:59:47 +0200

    refactor main component, add styles, add header, add data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import './App.css';
+import { useState } from "react";
+
+import "./App.scss"
+import ContentContainer from "./content/contentContainer/ContentContainer";
 
 function App() {
+  const [data, setData] = useState(require('./fe_data.json'));
+  const [direction, setDirection] = useState('request');
   return (
     <div className="App">
+      <header>
+        <div className="Title">
+          <div className="Method">
+            <h2>{data.method}</h2>
+            <h1>{data.path}</h1>
+          </div>
+          <h3>ALL APIS > mobile-api.dnssf.com > {data.path}</h3>
+        </div>
+        <div className="Divider"/>
+        <div className="TabContainer">
+          <div
+            className={`TabButton ${direction === "request"? "Active" : ""}`}
+            onClick={() => setDirection("request")}
+          >Request</div>
+          <div
+            className={`TabButton ${direction === "response"? "Active" : ""}`}
+               onClick={() => setDirection("response")}
+          >Response</div>
+        </div>
+      </header>
+      <div className="divider"/>
+      <ContentContainer data={data[direction]} setData={setData}/>
     </div>
   );
 }
