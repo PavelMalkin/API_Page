5ae84af655eb7ae405f6253974fbc4be05261adb
Pavel Malkin <frontenddev7@yieldx.app>
2021-11-22 13:43:56 +0200

    add filter function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import SearchBar from "./content/searchBar/SearchBar";
+import DataTable from "./content/dataTable/DataTable";
 
 import "./App.scss"
 
 function App() {
   const [data, setData] = useState(require('./fe_data.json'));
   const [direction, setDirection] = useState('request');
+  const [filter, setFilter] = useState({
+    name: "",
+    pii: false
+  });
+  const [filteredData, setFilteredData] = useState(data)
 
   const updateData = (value) => {
     const tempData = {...data};
     tempData[direction] = {...tempData[direction], ...value}
     setData(tempData)
   }
+
+  useEffect(() => {
+    if (filter.name || filter.pii) {
+
+      const filteredObject = (object, filterName, pii) => {
+        const result = {};
+        Object.keys(object)
+          .forEach(name => {
+            if (filterName && name.toLowerCase().includes(filterName) && !pii) {
+              result[name] = object[name];
+            } else {
+              object[name].forEach(paramType => {
+                if (
+                  (paramType.name.toLowerCase().includes(filterName)
+                    || name.toLowerCase().includes(filterName)
+                  )
+                  && (!pii || paramType.pii)
+                ) {
+                  if (!result[name]) result[name] = [];
+                  result[name].push(paramType);
+                }
+              });
+            }
+          });
+        return result;
+      };
+
+      const searchingName = filter.name.toLowerCase();
+
+      setFilteredData({
+        ...data,
+        request: filteredObject(data.request, searchingName, filter.pii ),
+        response: filteredObject(data.response, searchingName, filter.pii )
+      });
+
+    } else {
+      setFilteredData(data);
+    }
+  }, [data, filter])
 
   return (
     <div className="App">
