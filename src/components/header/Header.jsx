364cb974f1d91a7faf49789a437d7fd3997dbcde
Pavel Malkin <frontenddev7@yieldx.app>
2021-11-23 13:28:02 +0200

    fix header data

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = ({data, direction, setDirection}) => {
           <h2>{data.method}</h2>
           <h1>{data.path}</h1>
         </div>
-        <h3>ALL APIS > mobile-api.dnssf.com > {data.path}</h3>
+        <h3>ALL APIs > {data.api} > {data.path}</h3>
       </div>
       <div className="Divider"/>
       <div className="TabContainer">
